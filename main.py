67ae5c43d62c26655213dab6147d97eab140ecc3
George Huber <georgehuber8@gmail.com>
2024-06-13 10:00:10 -0400

    basic data window

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
 import numpy as np
 import netCDF4 as nc
 
-DEBUG = True
+DEBUG = False
 
 class App:
     def __init__(self, root):
@@ -17,16 +17,23 @@ class App:
         self.root.geometry("800x800")
         self.root.title("Panopoly who?")
 
+        self.frm = None
+        self.frm2 = None
+        self.curFrame = None
+
         self.filePath = ""
         self.data = None
         self.axis1 = tk.StringVar()
         self.axis2 = tk.StringVar()
         self.plotType = tk.StringVar()
 
+
         self.plotTypes = ["Line Plot", "Color Map"]
 
+        self.create_plot_frame()
+        # self.create_data_view_frame()
         self.set_main_menu()
-        self.create_widgets()
+        self.set_active(self.frm)
 
         if DEBUG:
             self.filePath = "/Users/grhuber/Downloads/2018_High_Vertical.geosgcm_gwd.20180201.nc4"
@@ -41,11 +48,16 @@ class App:
         self.fileMenu.add_separator()
         self.fileMenu.add_command(label="Quit", command=self.root.quit)
         self.menuBar.add_cascade(label="File", menu=self.fileMenu)
+
+        self.windowMenu = tk.Menu(self.menuBar, tearoff=0)
+        self.windowMenu.add_command(label="Plot", command=lambda: self.set_active(self.frm))
+        self.windowMenu.add_command(label="Data", command=lambda: self.set_active(self.frm2))
+        self.menuBar.add_cascade(label="Window", menu=self.windowMenu)
         self.root.config(menu=self.menuBar)
 
-    def create_widgets(self):
+    def create_plot_frame(self):
         self.frm = ttk.Frame(self.root)
-        self.frm.grid()
+        self.frm.grid(column=0, row=0,sticky ="nsew")
         ttk.Label(self.frm, text="Panopoly The Sequel", padding=10).grid(column=0, row=0)
 
         self.axis1Drop = ttk.OptionMenu(self.frm, self.axis1) 
@@ -57,6 +69,25 @@ class App:
         ttk.Button(self.frm, text="PLOT", command=self.plot).grid(column=0, row=2)
         self.plotTypeDrop = ttk.OptionMenu( self.frm, self.plotType, self.plotTypes[0], *self.plotTypes) 
         self.plotTypeDrop.grid(row=1, column=0)
+
+    def create_data_view_frame(self):
+        self.frm2 = ttk.Frame(self.root)
+        self.frm2.grid(column=0, row=0,sticky ="nsew")
+        ttk.Label(self.frm2, text="Data Properties", padding=10).grid(column=0, row=0)
+        ttk.Label(self.frm2, text="Metadata").grid()
+        if self.data:
+            for property in self.data.ncattrs():
+                ttk.Label(self.frm2, text=property).grid()
+                ttk.Label(self.frm2, text=self.data.getncattr(property)).grid()
+        ttk.Label(self.frm2, text="Variables").grid()
+        if self.data:
+            for variable in self.data.variables.keys():
+                ttk.Label(self.frm2, text=variable).grid()
+                try:
+                    ttk.Label(self.frm2, text=self.data.variables[variable].long_name).grid()
+                except:
+                    pass
+
     def select_file(self):
         self.filePath = askopenfilename()
         if self.filePath:
@@ -66,11 +97,12 @@ class App:
     def load_data(self):
         try:
             self.data = nc.Dataset(self.filePath)
-            self.update_properties_dropdown()
         except Exception as e:
             tk.messagebox.showerror("Error", f"Failed to load NetCDF file: {e}")
-            self.filePath = ""
-            self.data = None
+            return
+        self.update_properties_dropdown()
+        self.create_data_view_frame()
+        self.set_active(self.curFrame)
 
     def update_properties_dropdown(self):
         if self.data:
@@ -113,6 +145,10 @@ class App:
         canvas = FigureCanvasTkAgg(fig, master=self.frm)   
         canvas.draw()
         canvas.get_tk_widget().grid(row=3,column=0)
+    
+    def set_active(self, frm):
+        frm.tkraise()
+        self.curFrame = frm
 
 def main():
     matplotlib.use('agg')  
